a61750ee677bd5b9edf02bd73698c90179d8324e
elenakomaritska <komaritskaelena@gmail.com>
2020-09-18 14:44:29 +0300

    внесение правок

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,27 @@
 document.addEventListener("touchstart", function () {}, true);
 
-
 $(document).ready(function () {
 
     //Меню
 
     $('.menu-burger').on('click', function () {
-        $('.navigation__list').toggleClass('active').css('transition', '0.5s linear')
+        $('.header__navigation').toggleClass('active').css('transition', '0.5s linear')
         $('.header__overlay').toggleClass('active')
     });
 
-    $('.navigation__list a').on('click', function () {
-        $('.navigation__list').removeClass('active');
+    $('.header__navigation a , .navigation__link').on('click', function () {
+        $('.header__navigation').removeClass('active');
+        $('.header__overlay').removeClass('active')
     });
 
     $('.header__overlay').on('click', function () {
-        $('.navigation__list').removeClass('active')
+        $('.header__navigation').removeClass('active')
         $(this).removeClass('active')
     });
 
     if ($(window).width() >= 1024) {
         $('.header').removeClass('active');
     }
-
-
-
-
 
     //Попапы
     jQuery(function (a) {
@@ -44,11 +40,11 @@ $(document).ready(function () {
             $("#playerPopoup").attr('src', '')
         });
 
-    $('.link-video').on('click', function () {
-        $(".modall").removeClass('active-modall');
-        $(".modall-video").addClass('active-modall');
-        $('.body').addClass('showing-modal');
-    });
+    // $('.link-video').on('click', function () {
+    //     $(".modall").removeClass('active-modall');
+    //     $(".modall-video").addClass('active-modall');
+    //     $('.body').addClass('showing-modal');
+    // });
 
     jQuery(".only_number").keypress(function (e) {
         if (e.which != 8 && e.which != 0 && e.which != 46 && (e.which < 48 || e.which > 57)) {
